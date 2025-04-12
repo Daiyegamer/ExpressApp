d1e81d5006ec288edcc384bbdce61f4c283cd8b2
Adil surve <180300156+Daiyegamer@users.noreply.github.com>
2025-04-12 05:20:53 -0400

    add trust proxy

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.set("trust proxy", 1);
 app.use(sessions({
   secret: process.env.SESSIONSECRET,
   name: "MyUniqueSessID",
